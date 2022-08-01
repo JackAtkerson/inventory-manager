fded9ab68f8e80df0478459e4e5493cd6d491809
Jack Atkerson <jatkerson18@gmail.com>
2022-08-01 18:18:20 -0500

    Dashboard functionality

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,11 +4,54 @@ const { User, Warehouse, Category, Product } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-  res.render('dashboard', {loggedIn: true})
+  Warehouse.findAll({
+    attributes: [
+      'id',
+      'warehouse_name',
+      'description',
+      'user_id'
+    ],
+    include: {
+      model: User,
+      attributes: ['id', 'username']
+    }
+  })
+    .then(dbWarehouseData => {
+      const warehouses = dbWarehouseData.map(warehouses => warehouses.get({ plain: true }));
+      res.render('dashboard', { warehouses });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/warehouse/:id', (req, res) => {
-  res.render('single-warehouse', {loggedIn: true})
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name',
+      'warehouse_id'
+    ],
+    include: [
+      {
+        model: Warehouse,
+        attributes: ['id', 'warehouse_name']
+      },
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'quantity', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      const categories = dbCategoryData.map(categories => categories.get({ plain: true }));
+      res.render('single-warehouse', { categories });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.get('/create-warehouse', (req, res) => {
